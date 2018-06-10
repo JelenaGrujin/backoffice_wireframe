13bce8e54323ac6a80da4f7b812f4ed8f45de0ee
Jelena <39518565+JelenaGrujin@users.noreply.github.com>
2018-06-10 17:15:42 +0200

    Add files via upload

---
+++ forma za Losmija/panel.php
@@ -0,0 +1,82 @@
+<html>
+<head>
+<title>DEMO PANEL</title>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+<div id="header">
+<div class="lista">
+<a href="#">File</a>
+<a href="#">Edith</a>
+<a href="#">View</a>
+<a href="#">Help</a>
+</div>
+
+<div class="ikone">
+<a href="#"><img src="css/img/z648.jpg"></a>
+<a href="#"><img src="css/img/o648.jpg"></a>
+<a href="#"><img src="css/img/s648.jpg"></a>
+</div>
+
+</div>
+<div id="content">
+<div class="sidebar">
+<a href="#"><img src="css/img/1648.jpg"></a>
+<a href="#"><img src="css/img/2648.jpg"></a>
+<a href="#"><img src="css/img/3648.jpg"></a>
+<a href="#"><img src="css/img/4648.jpg"></a>
+<a href="#"><img src="css/img/5648.jpg"></a>
+
+
+</div>
+<div class="area1">
+<h1> Results</h1>
+</div>
+<div class="area2">
+<h1> Budggets</h1>
+</div>
+<div class="area3">
+<h1> Levels</h1>
+</div>
+<div>
+<table class="table">
+<tr>
+<td class="first">Person</td>
+<td class="first">Result</td>
+<td class="first">Team</td>
+<td class="first">Budgget</td>
+<td class="first">Level</td>
+<td class="first">Status</td>
+</tr>
+<tr>
+<td>First Person</td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+</tr>
+<tr>
+<td>Second Person</td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+</tr>
+<tr>
+<td>Third Person</td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+</tr>
+</table>
+</div>
+
+</div>
+</body>
+
+</html>
